0bdecce79f8e5b3e74643dfc842a163f49fe0bef
shimul <shimul@gmail.com>
2022-05-12 11:25:27 +0600

    dental treatment section

---
+++ src/Pages/Home/DentalCare/DentalCare.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import dental from '../../../assets/images/treatment.png'
+
+const DentalCare = () => {
+    return (
+        <div>
+            <div class="hero min-h-screen ">
+                <div class="hero-content flex-col lg:flex-row">
+                    <img src={dental} class="max-w-sm rounded-lg shadow-2xl" />
+                    <div>
+                        <h1 class="text-4xl font-bold">Exceptional Dental Care,on Your Terms!</h1>
+                        <p class="py-6">Dental Cosmetics Treatments In Los Angeles. Dental cosmetics
+                            are the application of scientific technology to improve our dental health, the
+                            functionality of our teeth, and oral aesthetics, translating into the improvement
+                            of the beauty of our smile. The dentist who specializes in this type of treatment
+                            is often known as a cosmetic dentist.</p>
+                        <button class="btn btn-primary text-white font-bold bg-gradient-to-r from-secondary to-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DentalCare;
