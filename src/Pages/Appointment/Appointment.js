fef318407424774a07dfdeb5ef27effe7930be3d
shimul <shimul@gmail.com>
2022-05-20 08:09:15 +0600

    servicePart added

--- src/Pages/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from '../Home/Shared/Footer/Footer';
 import AppointmentBanner from './AppointmentBanner';
 import AvailableAppointment from './AvailableAppointment';
 
 
 const Appointment = () => {
+    const [date, setDate] = useState(new Date());
     return (
         <div>
-            <AppointmentBanner></AppointmentBanner>
-            <AvailableAppointment></AvailableAppointment>
+            <AppointmentBanner date={date} setDate={setDate}></AppointmentBanner>
+            <AvailableAppointment date={date} setDate={setDate}></AvailableAppointment>
             <Footer></Footer>
         </div>
     );
--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import { format } from 'date-fns';
+import React, { useEffect, useState } from 'react';
+import ServicePart from '../ServicePart';
 
-const AvailableAppointment = () => {
+
+const AvailableAppointment = ({ date }) => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, [])
     return (
         <div>
-            Available Appointment on
+            <h4 className='text-xl text-center text-secondary'>Available Appointment on : {format(date, 'PP')}</h4>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+
+                {
+                    services.map(service => <ServicePart
+                        key={service._id}
+                        service={service}
+                    ></ServicePart>)
+                }
+            </div>
         </div>
     );
 };
