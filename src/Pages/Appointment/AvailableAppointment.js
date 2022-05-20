8b7d76fddb793b6df650c8744b3f43fcad84d5d3
shimul <shimul@gmail.com>
2022-05-20 21:04:14 +0600

    dynamic service added

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -1,28 +1,35 @@
 import { format } from 'date-fns';
 import React, { useEffect, useState } from 'react';
 import ServicePart from '../ServicePart';
+import BookingModal from './BookingModal';
 
 
 const AvailableAppointment = ({ date }) => {
     const [services, setServices] = useState([]);
-
+    const [treatment, setTreatment] = useState(null);
     useEffect(() => {
-        fetch('services.json')
+        fetch('http://localhost:5000/service')
             .then(res => res.json())
             .then(data => setServices(data));
     }, [])
     return (
         <div>
-            <h4 className='text-xl text-center text-secondary'>Available Appointment on : {format(date, 'PP')}</h4>
+            <h4 className='text-xl text-center text-secondary my-12'>Available Appointment on : {format(date, 'PP')}</h4>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
 
                 {
                     services.map(service => <ServicePart
                         key={service._id}
                         service={service}
+                        setTreatment={setTreatment}
                     ></ServicePart>)
                 }
             </div>
+            {treatment && <BookingModal
+                date={date}
+                setTreatment={setTreatment}
+                treatment={treatment}>
+            </BookingModal>}
         </div>
     );
 };
