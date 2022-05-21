f08bc2800c4d7691a0f606f89f6c6cc88d12ffbc
shimul <shimul@gmail.com>
2022-05-21 09:13:21 +0600

    envirnment variable and google singin part

--- src/Pages/Home/Login/Login.js
+++ src/Pages/Home/Login/Login.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    if (error) {
+        return (
+            <div>
+                <p>Error: {error.message}</p>
+            </div>
+        );
+    }
+    if (user) {
+        console.log(user);
+    }
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+    if (user) {
+        return (
+            <div>
+                <p>Signed In User: {user.email}</p>
+            </div>
+        );
+    }
     return (
-        <div>
-            <h3>Please Login </h3>
+        <div className='flex justify-center items-center h-screen'>
+            <div class="card w-96 bg-base-100 shadow-xl">
+                <div class="card-body">
+                    <h2 class="text-center text-2xl font-bold">Login!</h2>
+                    <div className='divider'>OR</div>
+                    <button
+                        onClick={() => signInWithGoogle()}
+                        class="btn btn-outline font-bold"
+                    >Continue With Google</button>
+
+                </div>
+            </div>
         </div>
     );
 };
